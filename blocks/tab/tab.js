5239b84c0685748a99846e8bb751a94bcd3cf84d
priya22-c <Priyanka.S22fc6d@cognizant.com>
2026-03-12 10:37:27 +0530

    Update tab.js

--- blocks/tab/tab.js
+++ blocks/tab/tab.js
@@ -8,10 +8,20 @@ export default function decorate(block) {
  
   /* create track */
   const track = document.createElement("div");
-  track.className = "tab-head-2";
+  track.className = "tab-head-1";
  
   const items = [...container.children];
  
   items.forEach((item) => {
     track.appendChild(item);
   });
+ 
+  /* clone items for infinite loop */
+  items.forEach((item) => {
+    const clone = item.cloneNode(true);
+    track.appendChild(clone);
+  });
+ 
+  container.appendChild(track);
+ 
+}
