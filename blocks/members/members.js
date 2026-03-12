8e44645ef19cbb4fd9f6b672abe9c8429c264c38
priya22-c <Priyanka.S22fc6d@cognizant.com>
2026-03-12 17:56:45 +0530

    Update members.js

--- blocks/members/members.js
+++ blocks/members/members.js
@@ -1,1 +1,21 @@
-
+export default function decorate(block) {
+  const container = block.children[0];
+  const membersWrapper = block.children[1];
+  if (!container || !membersWrapper) return;
+  /* main container */
+  container.classList.add("experts-container");
+  /* wrapper for members */
+  membersWrapper.classList.add("experts-wrapper");
+  const members = membersWrapper.children;
+  [...members].forEach((member) => {
+    member.classList.add("expert-card");
+    const img = member.querySelector("picture");
+    const name = member.querySelector("h3");
+    if (img) {
+      img.classList.add("expert-image");
+    }
+    if (name) {
+      name.classList.add("expert-name");
+    }
+  });
+}
