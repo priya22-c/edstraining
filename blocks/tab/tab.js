6ff0d6c4b0b0dd4143a5b2a0035f9aed6a43d4df
priya22-c <Priyanka.S22fc6d@cognizant.com>
2026-03-12 10:53:29 +0530

    Update tab.js

--- blocks/tab/tab.js
+++ blocks/tab/tab.js
@@ -1,13 +1,8 @@
 export default function decorate(block) {
  
-  const container = block.children[0];
-  if (!container) return;
+  const first = block.children[0];
+  if (!first) return;
+  first.classList.add('tab-head');
  
-  /* main class */
-  container.classList.add("nav-head-1");
- 
-  /* create track */
-  const track = document.createElement("div");
-  track.className = "nav-head-3";
- 
-}
+  const second = block.children[1];
+  if (second) second.classList.add('tab-head-2');
