0c6957bab3ae44338551337fae800f6a5b39800a
priya22-c <Priyanka.S22fc6d@cognizant.com>
2026-03-12 10:30:28 +0530

    Update tab.js

--- blocks/tab/tab.js
+++ blocks/tab/tab.js
@@ -1,1 +1,8 @@
-
+export default function decorate(block) {
+ 
+  const first = block.children[0];
+  if (!first) return;
+  first.classList.add('tab-head');
+ 
+  const second = block.children[1];
+  if (second) second.classList.add('tab-head-2');
